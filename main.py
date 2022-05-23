cfc21bb7969af9050ebbb8a24e4162bf89f20a55
Vik <victoriaapetrova@gmail.com>
2022-05-23 14:18:01 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# The Wanda Magic teapot virtual version. Just like the original it has 5 options that will guide your future. Just think of a question and see.
+import random
+answer=random.randint(1,5)
+if answer==1:
+    print("Yes • Да")
+if answer==2:
+    print("No • Не")
+if answer == 3:
+        print("Maybe • Може би")
+if answer == 4:
+        print("Not today • Не и днес")
+if answer == 5:
+        print("Who knows • Кой знае")
